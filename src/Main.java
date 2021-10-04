8043e192fe3c594fa27d8b3ed3b5cc7db3850afb
Davide Corsi <davide.corsi@par-tec.it>
2021-10-04 15:34:52 +0200

    first commit

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,12 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		System.out.print("Inserisci l'operazione da eseguire ");
-		String o = input.nextLine();
 		System.out.print("Inserisci il primo numero ");
 		int x = input.nextInt();
 		System.out.print("Inserisci il secondo numero ");
 		int y = input.nextInt();
+		input.nextLine();
+		System.out.print("Inserisci l'operazione da eseguire ");
+		String o = input.nextLine();
 	} 
 }
